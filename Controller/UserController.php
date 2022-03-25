630a4a6430fd7cdf94d055087dc2cddf2a80a3f6
Shebby91 <90932076+Shebby91@users.noreply.github.com>
2022-03-25 18:46:43 +0100

    added phpMailer and forgotten PW System

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -11,11 +11,20 @@ class UserController
 
     private $registrationValidData = array();
     private $registrationErrors = array();
-    private $registrationLabels = array("userName" => "Name", "userEmail" => "E-Mail-Adresse", "userPwd" => "Passwort", "userPwdRepeat" => "Passwort wiederholen");	
+    private $registrationLabels = array("userName" => "Benutzername", "userEmail" => "E-Mail-Adresse", "userPwd" => "Passwort", "userPwdRepeat" => "Passwort wiederholen");	
 
     private $loginValidData = array();
     private $loginErrors = array();
-    private $loginLabels = array("userEmail" => "E-Mail-Adresse", "userPwd" => "Passwort");	
+    private $loginLabels = array("userName" => "Benutzername", "userPwd" => "Passwort");	
+
+    private $resetPwdSecurityData = array();
+    private $resetPwdValidData = array();
+    private $resetPwdErrors = array();
+    private $resetPwdLabels = array("userPwd" => "Neues Passwort", "userPwdRepeat" => "Passwort wiederholen", "selector" => null, "validator" => null);	
+
+    private $requestPwdResetValidData = array();
+    private $requestPwdResetErrors = array();
+    private $requestPwdResetLabels = array("userEmail" => "E-Mail-Adresse");	
 
     public function __construct($view) 
     {
@@ -24,7 +33,11 @@ class UserController
     }
 
     public function showRegistrationForm()
-    {
+    {   
+        if(isset($_SESSION["userUid"])) {
+            header("location: ../index.php?controller=welcome&do=showWelcome");
+        }
+
         $this->view->setVars([
             'registrationLabels' => $this->registrationLabels,
             'registrationValidData' => $this->registrationValidData,
@@ -33,7 +46,11 @@ class UserController
     }    
 
     public function showLoginForm()
-    {
+    {   
+        if(isset($_SESSION["userUid"])) {
+            header("location: ../index.php?controller=welcome&do=showWelcome");
+        }
+
         $this->view->setVars([
             'loginLabels' => $this->loginLabels,
             'loginValidData' => $this->loginValidData,
@@ -41,9 +58,41 @@ class UserController
         ]);
     }
 
-    public function showRegistrationConfirmation()
+    public function showResetPasswordForm()
     {
-        
+        if(!isset($_GET["selector"]) || !isset($_GET["validator"])) {
+            new \deinBerichtsheft\Library\ErrorMsg('Deine Anfrage ist nicht valide. Bitte wende dich an den Support oder versuche es erneut.'); 
+            die;
+        } else if (empty($_GET["selector"]) || empty($_GET["validator"])){
+            new \deinBerichtsheft\Library\ErrorMsg('Deine Anfrage ist nicht valide. Bitte wende dich an den Support oder versuche es erneut.'); 
+            die;
+        } else if(ctype_xdigit($_GET["selector"]) !== true ||  ctype_xdigit($_GET["validator"]) !== true) {
+            new \deinBerichtsheft\Library\ErrorMsg('Deine Anfrage ist nicht valide. Bitte wende dich an den Support oder versuche es erneut.'); 
+            die;
+        } else {
+            $this->resetPwdSecurityData["selector"] =  $_GET["selector"];
+            $this->resetPwdSecurityData["validator"] =  $_GET["validator"];
+            $this->view->setVars([
+                'resetPwdLabels' => $this->resetPwdLabels,
+                'resetPwdValidData' => $this->resetPwdValidData,
+                'resetPwdErrors' => $this->resetPwdErrors,
+                'resetPwdSecurityData' => $this->resetPwdSecurityData
+            ]);
+        }
+
+    }
+
+    public function showRequestPasswordResetForm()
+    {
+        if(isset($_SESSION["userUid"])) {
+            header("location: ../index.php?controller=welcome&do=showWelcome");
+        }
+
+        $this->view->setVars([
+            'requestPwdResetLabels' => $this->requestPwdResetLabels,
+            'requestPwdResetValidData' => $this->requestPwdResetValidData,
+            'requestPwdResetErrors' => $this->requestPwdResetErrors
+        ]);
     }
 
     public function showLoginConfirmation()
@@ -51,12 +100,52 @@ class UserController
         if(!isset($_SESSION["userUid"])) {
             header("location: ../index.php?controller=User&do=showLoginForm");
         }
+
+        if(isset($_SESSION["loginConfirmationShown"])) {
+            header("location: ../index.php?controller=welcome&do=showWelcome");
+        }
+
+        $_SESSION["loginConfirmationShown"] = true;
     }
 
-    function showLogout() {
+
+
+    public function showRegistrationConfirmation()
+    {       
+        if(!isset($_SESSION["registeredSuccessfully"])) {
+            header("location: ../index.php?controller=User&do=showLoginForm");
+        }
+
+        session_unset();
+        session_destroy();
+    }
+
+    public function showResetPasswordConfirmation()
+    {
+        if(!isset($_SESSION["pwdResetSuccessfull"])) {
+            header("location: ../index.php?controller=User&do=showLoginForm");
+        }
+
+        session_unset();
+        session_destroy();
+    }
+    
+    public function showRequestPasswordResetConfirmation()
+    {
+        if(!isset($_SESSION["pwdResetReqeuestSuccessfull"])) {
+            header("location: ../index.php?controller=User&do=showLoginForm");
+        }
+
+        session_unset();
+        session_destroy();
+    }
+
+    function showLogoutConfirmation() {
+        
         if(!isset($_SESSION["userUid"])) {
             header("location: ../index.php?controller=User&do=showLoginForm");
         }
+
         session_unset();
         session_destroy();
     }
@@ -64,15 +153,23 @@ class UserController
     public function validateRegistrationForm(){
         foreach ($this->registrationLabels as $index => $value) {
             if (!isset($_POST[$index]) || empty($_POST[$index])) {
+                if(!isset($_POST["userName"]) || empty($_POST["userName"]) || !isset($_POST["userEmail"]) || empty($_POST["userEmail"])) {
+                    $this->registrationErrors["userName"] = "Bitte Benutzernamen und E-Mail-Adresse angeben.";
+                    $this->registrationErrors["userEmail"] = "Bitte Benutzernamen und E-Mail-Adresse angeben.";
+                }
                 $this->registrationErrors[$index] = "Bitte " . $value . " angeben";
+            } else if ($index == "userName" && $this->db->checkUserNameExist($_POST[$index])){
+                $this->registrationErrors[$index] = "Benzutzername oder E-Mail-Adresse bereits vergeben.";
+                $this->registrationErrors["userEmail"] = "Benzutzername oder E-Mail-Adresse bereits vergeben.";
             } else if ($index == "userEmail" && !filter_var($_POST[$index], FILTER_VALIDATE_EMAIL)){
                 $this->registrationErrors[$index] = "E-Mail-Adresse ist ung&uuml;ltig";
             } else if ($index == "userEmail" && $this->db->checkEmailExist($_POST[$index])){
-                $this->registrationErrors[$index] = "E-Mail-Adresse existiert bereits. Bitte andere E-Mail-Adresse angeben.";
+                $this->registrationErrors[$index] = "Benzutzername oder E-Mail-Adresse bereits vergeben.";
+                $this->registrationErrors["userName"] = "Benzutzername oder E-Mail-Adresse bereits vergeben.";
             } else if ($index == "userPwd" && strlen($_POST[$index]) <= 7) {
                 $this->registrationErrors[$index] = "Passwort ist zu kurz. Bitte mindestens 8 Zeichen verwenden.";
             } else if ($index == "userPwdRepeat") {
-                if($_POST[$index] !== $_POST["userPwd"]){
+                if($_POST[$index] !== $_POST["userPwd"]) {
                     $this->registrationErrors[$index] = "Passwörter stimmen nicht überein.";
                 } 
             } else {
@@ -87,6 +184,9 @@ class UserController
             if ($this->db->setUser($this->registrationValidData)) {
                 $this->view->setDoMethodName("showRegistrationConfirmation");
                 $this->showRegistrationConfirmation();
+            } else{
+                new \deinBerichtsheft\Library\ErrorMsg('Bei deiner Registrierung ist ein Fehler aufgetreten. Bitte versuche es erneut oder wende dich an den Support.'); 
+                die;
             }
         }
     }
@@ -94,11 +194,15 @@ class UserController
     public function validateLoginForm(){
         foreach ($this->loginLabels as $index => $value) {
             if (!isset($_POST[$index]) || empty($_POST[$index])) {
-                $this->loginErrors[$index] = "Bitte " . $value . " angeben";
-            } else if ($index == "userEmail" && !filter_var($_POST[$index], FILTER_VALIDATE_EMAIL)){
-                $this->loginErrors[$index] = "E-Mail-Adresse ist ung&uuml;ltig.";
-            } else if ($index == "userPwd" && !$this->db->checkPwd(array("userPwd" => $_POST[$index], "userEmail" => $_POST["userEmail"]))) {
-                $this->loginErrors[$index] = "E-Mail-Adresse oder Passwort falsch.";
+                if(!isset($_POST["userName"]) || empty($_POST["userName"]) || !isset($_POST["userPwd"]) || empty($_POST["userPwd"])) {
+                    $this->loginErrors["userName"] = "Bitte Benutzernamen und Passwort angeben.";
+                    $this->loginErrors["userPwd"] = "Bitte Benutzernamen und Passwort angeben.";
+                }
+            } else if ($index == "userName" && !$this->db->checkUserNameExist($_POST[$index])){
+                $this->loginErrors[$index] = "Benutzername oder Passwort falsch.";
+            } else if ($index == "userPwd" && !$this->db->checkPwd(array("userPwd" => $_POST[$index], "userName" => $_POST["userName"]))) {
+                $this->loginErrors["userName"] = "Benutzername oder Passwort falsch.";
+                $this->loginErrors[$index] = "Benutzername oder Passwort falsch.";
             } else {
                 $this->loginValidData[$index] = $_POST[$index];
             }       
@@ -108,11 +212,76 @@ class UserController
             $this->view->setDoMethodName("showLoginForm");
             $this->showLoginForm();
         } else {
-            if($this->db->getUser($this->loginValidData["userEmail"])) {
+            if(!$this->db->getUser($this->loginValidData["userName"])) {
+                new \deinBerichtsheft\Library\ErrorMsg('Bei deinem Login ist ein Fehler aufgetreten. Bitte versuche es erneut oder wende dich an den Support.'); 
+                die;           
+            } else{
                 $this->view->setDoMethodName("showLoginConfirmation");
                 $this->showLoginConfirmation();
+            }
+        }
+    }
+
+    public function validateResetPasswordForm(){
+        foreach ($this->resetPwdLabels as $index => $value) {
+            if ($index == "userPwd" && empty($_POST["userPwd"])){
+                $this->resetPwdErrors[$index] = $value . " angeben";
+            } else if ($index == "userPwdRepeat" && empty($_POST["userPwdRepeat"])){
+                $this->resetPwdErrors[$index] = $value . " angeben";
+            } else if ($index == "userPwd" && strlen($_POST[$index]) <= 7 || $index == "userPwdRepeat" && strlen($_POST[$index]) <= 7) {
+                $this->resetPwdErrors[$index] = "Passwort ist zu kurz. Bitte mindestens 8 Zeichen verwenden.";
+            } else if ($index == "userPwdRepeat") {
+                if($_POST[$index] !== $_POST["userPwd"]) {
+                    $this->resetPwdErrors[$index] = "Passwörter stimmen nicht überein.";
+                    $this->resetPwdErrors["userPwd"] = "Passwörter stimmen nicht überein.";
+                }
+            } else if ($index == "selector" && empty($_POST["selector"])){
+                $this->resetPwdErrors[$index] = "Security Error";
+            } else if ($index == "validator" && empty($_POST["validator"])){
+                $this->resetPwdErrors[$index] = "Security Error";
+            } else {
+                $this->resetPwdValidData[$index] = $_POST[$index];
+            }    
+        }
+
+        if (count($this->resetPwdErrors) > 0) {
+            $this->view->setVars(["selector" => $this->resetPwdValidData["selector"], "validator" => $this->resetPwdValidData["validator"]]);
+            $this->view->setDoMethodName("showResetPasswordForm");
+            $this->showResetPasswordForm();
+        } else {
+            if (!$this->db->setNewPwd($this->resetPwdValidData)) {
+                new \deinBerichtsheft\Library\ErrorMsg('Deine Anfrage ist nicht valide. Bitte wende dich an den Support oder versuche es erneut.'); 
+                die;
+            } else {
+                $this->view->setDoMethodName("showResetPasswordConfirmation");
+                $this->showResetPasswordConfirmation();      
+            }
+        }  
+    }
+
+    public function validateRequestPwdResetForm(){
+        foreach ($this->requestPwdResetLabels as $index => $value) {
+            if (!isset($_POST[$index]) || empty($_POST[$index])) {
+                $this->requestPwdResetErrors[$index] = $value . " angeben.";
+            } else if ($index == "userEmail" && !filter_var($_POST[$index], FILTER_VALIDATE_EMAIL)){
+                $this->requestPwdResetErrors[$index] = "E-Mail-Adresse ist ung&uuml;ltig";
+            } else if ($index == "userEmail" && !$this->db->checkEmailExist($_POST[$index])){
+                $this->requestPwdResetErrors[$index] = "E-Mail-Adresse ist ung&uuml;ltig";
+            } else {
+                $this->requestPwdResetValidData[$index] = $_POST[$index];
+            }       
+        }
+
+        if (count($this->requestPwdResetErrors) > 0) {
+            $this->view->setDoMethodName("showRequestPasswordResetForm");
+            $this->showRequestPasswordResetForm();
+        } else {
+            if(!$this->db->generateResetToken($this->requestPwdResetValidData["userEmail"])) {
+                new \deinBerichtsheft\Library\ErrorMsg('Deine Anfrage ist nicht valide. Bitte wende dich an den Support oder versuche es erneut.'); 
+                die;
             } else{
-                new \deinBerichtsheft\Library\ErrorMsg('Bei deinem Login ist ein Fehler Aufgetreten. Bitte versuche es erneut oder wende dich an den Support.'); 
+                $this->view->setDoMethodName("showRequestPasswordResetConfirmation");
+                $this->showRequestPasswordResetConfirmation();
             }
         }
     }
--- Model/UserModel.php
+++ Model/UserModel.php
@@ -4,6 +4,12 @@ namespace deinBerichtsheft\Model;
 use PDO;
 use PDOException;
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 class UserModel extends Database
 {
     public function checkEmailExist($values)
@@ -31,15 +37,40 @@ class UserModel extends Database
         return false;
     }
 
+    public function checkUserNameExist($values)
+    {
+        $pdo = $this->linkDB();
+
+        try {
+            $sth = $pdo->prepare('SELECT userName FROM user WHERE userName = ?;');
+            $sth->execute(array($values));
+        } catch (PDOException $e) {
+            new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Abrufen der Daten.", $e); 
+            die;
+        }
+
+        if($sth->rowCount() == 0){
+            return false;
+        }
+
+        $userName = $sth->fetchAll(PDO::FETCH_ASSOC);
+        
+        if($userName[0]["userName"] == $values){
+            return true;
+        }
+  
+        return false;
+    }
+
     public function checkPwd($values)
     {
-        $sql = "Select userPwd FROM user WHERE userEmail = :email;";
+        $sql = "Select userPwd FROM user WHERE userName = :name;";
 
         $pdo = $this->linkDB();
 
         try {
             $sth = $pdo->prepare($sql);
-            $sth->execute(array(":email" => $values["userEmail"]));
+            $sth->execute(array(":name" => $values["userName"]));
         } catch (PDOException $e) {
             new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Abrufen der Daten.", $e); 
             die;
@@ -65,7 +96,7 @@ class UserModel extends Database
         $pdo = $this->linkDB();
 
         try {
-            $sth = $pdo->prepare('SELECT * FROM user WHERE userEmail = ?;');
+            $sth = $pdo->prepare('SELECT * FROM user WHERE userName = ?;');
             $sth->execute(array($values));
         } catch (PDOException $e) {
             new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Abrufen der Daten.", $e); 
@@ -91,6 +122,203 @@ class UserModel extends Database
         return false;
     }
 
+    public function generateResetToken($values)
+    {
+
+        $pdo = $this->linkDB();
+
+        $selector =bin2hex(random_bytes(8));
+
+        $token = random_bytes(32);
+
+        $url = "http://localhost/index.php?controller=User&do=showResetPasswordForm&selector=" . $selector . "&validator=". bin2hex($token);
+
+        $expires = date("U") + 1800;
+
+        $sql = "DELETE FROM pwdReset WHERE pwdResetEmail = ? ;";
+    
+        try {
+            $sth = $pdo->prepare($sql);
+            $sth->execute(array($values));
+            $sth = null;
+        } catch (PDOException $e) {
+            new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Schreiben der Daten.", $e); 
+            die;
+        }
+
+        $sql = "INSERT INTO pwdReset (`pwdResetEmail`, `pwdResetSelector`, `pwdResetToken`, `pwdResetExpires`) VALUES (
+            :email, :selector, :token, :expires);";
+
+        $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+
+        try {
+            $sth = $pdo->prepare($sql);
+            $sth->execute(array(
+                ":email" => $values,
+                ":selector" => $selector,
+                ":token" => $hashedToken,
+                ":expires" => $expires));
+            $sth = null;
+        } catch (PDOException $e) {
+            new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Schreiben der Daten.", $e); 
+            die;
+        }
+        
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            //Enable verbose debug output $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'deinberichtsheftde@gmail.com';                     //SMTP username
+            $mail->Password   = 'dB.deietS';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;             //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        
+            //Recipients
+            $mail->setFrom('deinberichtsheftde@gmail.com', 'deinBerichtsheft.de');   
+            $mail->addAddress('sgrauthoff@gmail.com');               //Name is optional
+            $mail->addReplyTo('deinberichtsheftde@gmail.com', 'Support');
+            $mail->ContentType;
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Passwort zur&uuml;cksetzen';
+            $mail->Body    = '<html lang="en"><head>
+              <!-- Required meta tags -->
+              <meta charset="utf-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1">    
+              <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            </head>
+            <body class="d-flex flex-column h-100">
+             
+              <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary shadow">
+                <div class="container d-flex justify-content-center px-4">
+                    <span class="navbar-brand">deinBerichtsheft.de</span>
+                </div>
+            </nav>
+            <main class="flex-shrink-0">
+              <div class="container">
+                <div class="card shadow text-center mt-3">
+                    <div class="card-body">
+                      <h5 class="card-title">Passwort vergessen?</h5>
+                      <p class="card-text">Kein Problem, hier kannst du dein Passwort ändern:</p>
+                      <a class="btn btn-primary" href=' . $url . '>Passwort ändern</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        deinBerichtsheft.de
+                    </div>
+                </div>
+              </div>
+              </main>
+              <footer class="footer mt-auto py-3 px-4 bg-primary shadow-footer">
+                <div class="container d-flex justify-content-between">
+                  <a class="text-white text-decoration-none" href="#">Impressum</a>
+                  <a class="text-white text-decoration-none" href="#">Datenschutz</a>
+                </div>
+              </footer>
+              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+            </body></html>';
+
+        //$mail->send();
+        var_dump($url);
+  
+        } catch (PDOException $e) {
+            new \deinBerichtsheft\Library\ErrorMsg("E-Mail konnte nicht gesendet werden. {$mail->ErrorInfo}"); 
+            die;
+        }
+
+        $_SESSION["pwdResetReqeuestSuccessfull"] = true;
+        return true;
+
+    }
+
+    public function setNewPwd($values)
+    {   
+        $pdo = $this->linkDB();
+        $currentDate = date("U");
+        try {
+            $sth = $pdo->prepare('SELECT * FROM pwdReset WHERE pwdResetSelector = :selector AND pwdResetExpires >= :currentDate;');
+            $sth->execute(array(
+                ":selector" => $values["selector"],
+                ":currentDate" => $currentDate));
+        } catch (PDOException $e) {
+            new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Abrufen der Daten.", $e); 
+            die;
+        }
+
+        if($sth->rowCount() == 0){
+            return false;
+        }
+
+        $securityData = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        if($securityData == null || empty($securityData)) {
+            return false;
+        } else {
+
+            $tokenBin = hex2bin($values["validator"]);
+            $tokenCheck = password_verify($tokenBin, $securityData[0]["pwdResetToken"]);
+
+            if($tokenCheck == false) {
+                return false;
+            } else if ($tokenCheck == true) {
+                try {
+                    $sth = $pdo->prepare('SELECT * FROM user WHERE userEmail = :email;');
+                    $sth->execute(array(":email" => $securityData[0]["pwdResetEmail"]));
+                } catch (PDOException $e) {
+                    new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Abrufen der Daten.", $e); 
+                    die;
+                }
+
+                if($sth->rowCount() == 0){
+                    return false;
+                }
+        
+                $user = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+                if ($user == null || empty($user)) {
+                    return false;
+                }
+                
+                $newHashedPwd = password_hash($values["userPwd"], PASSWORD_DEFAULT);
+
+                try {
+                    $sth = $pdo->prepare('UPDATE user SET userPwd = :pwd WHERE userEmail = :email;');
+                    $sth->execute(array(":pwd" => $newHashedPwd, ":email" => $user[0]["userEmail"]));
+                } catch (PDOException $e) {
+                    new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Abrufen der Daten.", $e); 
+                    die;
+                }
+
+                try {
+                    $sth = $pdo->prepare('DELETE FROM pwdReset WHERE pwdResetEmail = :email;');
+                    $sth->execute(array(":email" => $user[0]["userEmail"]));
+                } catch (PDOException $e) {
+                    new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Abrufen der Daten.", $e); 
+                    die;
+                }
+
+                try {
+                    $sth = $pdo->prepare('SELECT * FROM pwdReset WHERE pwdResetEmail = :email;');
+                    $sth->execute(array(":email" => $user[0]["userEmail"]));
+                } catch (PDOException $e) {
+                    new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Abrufen der Daten.", $e); 
+                    die;
+                }
+
+                if($sth->rowCount() !== 0){
+                    return false;
+                }
+
+                $_SESSION["pwdResetSuccessfull"] = true;
+                return true;
+            }
+        }
+    }
+
     public function setUser($values)
     {
         $guid = $this->createUUID();
@@ -114,7 +342,9 @@ class UserModel extends Database
             new \deinBerichtsheft\Library\ErrorMsg("Fehler beim Schreiben der Daten.", $e); 
             die;
         }
-                
+        
+        $_SESSION["registeredSuccessfully"] = true;
+
         return true;
     }
 }
